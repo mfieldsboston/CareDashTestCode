a8a09455428d389c096e1afcf5283d5a300872e5
mfieldsboston <mfields11@gmail.com>
2018-11-09 13:46:13 -0500

    edit to general styling and functionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,24 +1,54 @@
 $(document).ready(function(){
 
+    /* NAV START */
+
     $('.dropdown-toggle').click(function(e){
 
       $(this).siblings('.dropdown').toggle();
-
       $('.dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
 
     });
 
     $('html').click(function() {
+
       $('.dropdown').hide();
+
     });
 
     $('#nav-toggle').on('click', function() {
+
       this.classList.toggle('active');
+
     });
 
     $('#nav-toggle').click(function() {
+
         $('nav.desktop').toggle();
+
     });
+
+    /* NAV END */
+
+
+    /* FORM START */
+
+    /*$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+        e.preventDefault();
+
+    });*/
+
+    $('.tab a').on('click', function (e) {
+
+        e.preventDefault();
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        target = $(this).attr('href');
+        $('.tab-content > div').not(target).hide();
+        $(target).fadeIn(600);
+
+      });
+
+    /* FORM END */
 
 });
